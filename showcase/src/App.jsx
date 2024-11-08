95e1cb18b0c84e51c74d45d4ffc6310142d0a500
Mats <mats@jawlock.com>
2024-11-08 15:10:22 +0100

    first two projects added

--- showcase/src/App.jsx
+++ showcase/src/App.jsx
@@ -12,23 +12,40 @@ function App() {
       .catch((error) => console.error('Error loading projects:', error));
   }, []);
 
+  // Funktion för att returnera en färgad tag
+  function tags(tag, i){
+    switch(tag) {
+      case "HTML":
+        return <span key={i} style={{ color: 'yellow' }}>{tag}</span>
+      case "CSS":
+        return <span key={i} style={{ color: 'pink' }}> {tag} </span>
+      case "JS":
+        return <span key={i} style={{ color: 'lightblue' }}> {tag} </span>
+      case "React":
+        return <span key={i} style={{ color: 'purple' }}> {tag} </span>
+      default:
+        return <span key={i} style={{ color: 'lightgrey' }}> {tag} </span>
+    }
+  }
+
   return (
     <div className="project-list">
-      <ul className='flex'>
-      {projects.map((project) => (
-        <li key={project.id} className='card' target="_blank" rel="noopener noreferrer">
-
-            <a href={project.link} target='_blank'><img src={project.image} alt={project.title}/></a>
-            <div className='p-4'> 
-            <h2>{project.title}</h2>
-            {project.tags.map((tag, i) => {
-              return <span key={i}> {tag} </span>
-            })}
-            <p>{project.description}</p>
-          </div>
-        </li>
-      ))}
-       </ul>
+      <ul className="flex flex-wrap">
+        {projects.map((project) => (
+          <li key={project.id} className="card" target="_blank" rel="noopener noreferrer">
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <img src={project.image} alt={project.title} />
+            </a>
+            <div className="p-4"> 
+              <h2>{project.title}</h2>
+              <div>
+                {project.tags.map((tag, i) => tags(tag, i))}
+              </div>
+              <p>{project.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
